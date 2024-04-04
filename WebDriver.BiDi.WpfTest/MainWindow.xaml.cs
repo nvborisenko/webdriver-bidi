67227c16070cd4e47fafca587a9a6cfc572beac2
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2024-04-04 23:51:21 +0300

    Add wpf test

---
+++ WebDriver.BiDi.WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi;
+using OpenQA.Selenium.Chrome;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebDriver.BiDi.WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                var options = new ChromeOptions
+                {
+                    UseWebSocketUrl = true,
+                    //BrowserVersion = "121.0"
+                };
+
+                using var driver = new ChromeDriver(options);
+
+                using var bidi = await BiDiSession.ConnectAsync(((IHasCapabilities)driver).Capabilities.GetCapability("webSocketUrl").ToString()!);
+
+                using var context = await bidi.CreateBrowsingContextAsync();
+
+                context.NavigationStarted += async args => { await Task.Delay(1000); Console.WriteLine($"{DateTime.Now} {args}"); };
+                context.NavigationStarted += args => { Thread.Sleep(1000); Console.WriteLine($"{DateTime.Now} {args}"); return Task.CompletedTask; };
+
+                await context.NavigateAsync("https://selenium.dev");
+            });
+        }
+    }
+}
