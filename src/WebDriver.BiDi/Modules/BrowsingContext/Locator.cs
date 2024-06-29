cf28ee4f5abce77f0005bb3adae426cb9af2952a
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2024-06-29 21:57:39 +0300

    Locator discriminator

--- src/WebDriver.BiDi/Modules/BrowsingContext/Locator.cs
+++ src/WebDriver.BiDi/Modules/BrowsingContext/Locator.cs
@@ -2,35 +2,30 @@
 
 namespace OpenQA.Selenium.BiDi.Modules.BrowsingContext;
 
-[JsonDerivedType(typeof(CssLocator))]
-[JsonDerivedType(typeof(InnerTextLocator))]
-[JsonDerivedType(typeof(XPathLocator))]
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(CssLocator), "css")]
+[JsonDerivedType(typeof(InnerTextLocator), "innerText")]
+[JsonDerivedType(typeof(XPathLocator), "xpath")]
 public abstract class Locator
 {
-    public abstract string Type { get; }
-
     public static CssLocator Css(string value)
-        => new() { Value = value };
+        => new(value);
 
     public static InnerTextLocator InnerText(string value, bool? ignoreCase = default, MatchType? matchType = default, uint? maxDepth = default)
-        => new() { Value = value, IgnoreCase = ignoreCase, MatchType = matchType, MaxDepth = maxDepth };
+        => new(value) { IgnoreCase = ignoreCase, MatchType = matchType, MaxDepth = maxDepth };
 
     public static XPathLocator XPath(string value)
-        => new() { Value = value };
+        => new(value);
 }
 
-public class CssLocator : Locator
+public class CssLocator(string value) : Locator
 {
-    public override string Type => "css";
-
-    public string Value { get; set; }
+    public string Value { get; } = value;
 }
 
-public class InnerTextLocator : Locator
+public class InnerTextLocator(string value) : Locator
 {
-    public override string Type => "innerText";
-
-    public string Value { get; set; }
+    public string Value { get; } = value;
 
     public bool? IgnoreCase { get; set; }
 
@@ -45,9 +40,7 @@ public enum MatchType
     Partial
 }
 
-public class XPathLocator : Locator
+public class XPathLocator(string value) : Locator
 {
-    public override string Type => "xpath";
-
-    public string Value { get; set; }
+    public string Value { get; } = value;
 }
